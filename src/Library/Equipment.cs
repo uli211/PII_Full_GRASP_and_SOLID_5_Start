abe8feb0a3c1fa983dd5c57fcf57a3bfa65e98d5
Ulises <ulises.rattin@gmail.com>
2024-10-23 17:52:47 -0300

    Implementaciones

--- src/Library/Equipment.cs
+++ src/Library/Equipment.cs
@@ -8,14 +8,13 @@ namespace Full_GRASP_And_SOLID
 {
     public class Equipment
     {
-        public Equipment(string description, double hourlyCost)
+        public string Name { get; set; }
+        public int HourlyCost { get; set; }
+
+        public Equipment(string name, int hourlyCost)
         {
-            this.Description = description;
-            this.HourlyCost = hourlyCost;
+            Name = name;
+            HourlyCost = hourlyCost;
         }
-
-        public string Description { get; set; }
-
-        public double HourlyCost { get; set; }
     }
 }
